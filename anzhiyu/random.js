ec5669dd72e6c6bd5329eada9ea521b70f1040b2
f1234ghsdf <452855314@qq.com@qq.com>
2024-05-21 21:39:04 +0800

    Site updated: 2024-05-21 21:39:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/05/Unity第一人称移动系统/","2024/05/16/Unity脚本生命周期/","2024/05/05/标题/","2024/05/05/标题copy/"];function toRandomPost(){
+var posts=["2024/05/05/Unity第一人称移动系统/","2024/05/16/Unity脚本生命周期/","2024/05/05/标题/","2024/05/05/标题copy/","2024/05/05/UE5仿鬼泣Demo/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
